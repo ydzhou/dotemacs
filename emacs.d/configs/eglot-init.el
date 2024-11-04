e9a8f8b413ff8ccd5c4abfab77bef7c6d6bb357f
ydzhou <yd.zhou@me.com>
2024-11-03 22:59:47 -0800

    Update configs 1. Add python venv support 2. Update build files

--- emacs.d/configs/eglot-init.el
+++ emacs.d/configs/eglot-init.el
@@ -1,21 +1,23 @@
+(defun custom-eglot-configure-python ()
+  (setq venvPath "./venv/")
+  (setq eglot-workspace-configuration
+        (list (cons ':python (list ':venvPath venvPath ':pythonPath (concat venvPath "bin/python")))))  
+  (eglot-ensure)
+    )
+
 (use-package eglot
   :ensure t
   :hook
   (go-mode . eglot-ensure)
-  (python-mode . eglot-ensure)
+  (python-mode . custom-eglot-configure-python)
   (typescript-mode . eglot-ensure)
   (c-mode . eglot-ensure)
   (c++-mode . eglot-ensure)
+  (lisp-mode . eglot-ensure)
   :config
   (setq eglot-events-buffer-size 0
         eglot-sync-connect 2
         eglot-autoshutdown t)
   )
-
-;; (use-package eglot-booster
-;;   :after eglot
-;;   :config
-;;   (eglot-booster-mode)
-;;   )
 
 (provide 'eglot-init)
